ba5a787af0e929fb6a64cec60c7de9e4bcdf4c3d
Kuntash <kuntashtashi11@gmail.com>
2021-06-27 17:03:54 +0530

    Done with navigation menu, perfected some animations based on scroll

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+'use strict'
 //Navigation Bar Animation
+const nav = document.querySelector('.nav');
+const navContainer = document.querySelector('.nav--container');
+const initialYoffset = 0;
+document.addEventListener('scroll', ()=>{
+    const currentYoffset = window.pageYOffset;
+    const scrolledLength = currentYoffset - initialYoffset;
+    if (scrolledLength > 100)
+    {
+        nav.classList.add('NAV-ON-SCROLL');
+        navContainer.classList.add('CONTAINER-ON-SCROLL');
+    }
+
+    else if(scrolledLength < 100)
+    {
+        nav.classList.remove('NAV-ON-SCROLL');
+        navContainer.classList.remove('CONTAINER-ON-SCROLL');
+    }
+});
